74a0fbe714283f67daa3c044c6f296f664dcad47
flutter-lord <richardolojede2@gmail.com>
2025-10-29 21:23:38 +0100

    commit

---
+++ min_and_max_numbers.py
@@ -0,0 +1,5 @@
+num1, num2, num3, num4 = eval(input("Four numbers separated by a comma : "))
+
+max_num = max(num1, num2, num3, num4)
+min_num = min(num1, num2, num3, num4)
+print(max_num, "is the maximum number and", min_num, "is the minimum number" )
