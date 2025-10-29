970d234c3ed9ba6965a793757dd76177886b90df
flutter-lord <richardolojede2@gmail.com>
2025-10-29 22:05:43 +0100

    commit

---
+++ circle_points.py
@@ -0,0 +1,28 @@
+x1, y1 = eval(input("Enter the point of the circle x, y : "))
+radius = eval(input("Enter the radius of the circle : "))
+x2, y2 = eval(input("Enter the point x, y : "))
+
+import turtle as t
+t.penup()
+t.goto(x1, y1 - radius)
+t.pendown()
+t.circle(radius)
+
+t.penup()
+t.goto(x2, y2)
+t.pendown()
+t.begin_fill()
+t.fillcolor("red")
+t.circle(3)
+t.end_fill()
+
+t.penup()
+t.goto(x1 - 70, y1 - radius - 20)
+t.pendown()
+import math as m
+distance = m.sqrt(m.pow(x2 - x1, 2) + m.pow(y2 - y1, 2))
+t.write("The point is inside the circle" if distance <= radius  else \
+        "The point is outside the circle" )
+
+t.hideturtle()
+t.done()
