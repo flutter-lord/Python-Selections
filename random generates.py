07a247b19b66158d445e87bf20a23d03d0272e13
flutter-lord <richardolojede2@gmail.com>
2025-10-30 16:49:20 +0100

    commit

--- random generates.py
+++ random generates.py
@@ -1,8 +1,17 @@
 import random
-your_number = eval(input("Enter the number you guess fro 10 - 99: "))
+your_number = eval(input("Enter the sum of the number (1 - 18) : "))
 
 random_number = random.randint(10, 99)
 print(random_number, "is the original number")
+
+random_number_dig1 = random_number // 10
+random_number_dig2 = random_number % 10
+
+random_sum = random_number_dig1 + random_number_dig2
+print(random_sum,"is the sum of the integers in the original number\n")
+
+print("You guessed correctly" if your_number == random_sum else "You guessed wrongly")
+
 
 
 
