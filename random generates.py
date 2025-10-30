0c96b684a22c9336551c876e78775591cee0c9f7
flutter-lord <richardolojede2@gmail.com>
2025-10-30 16:42:41 +0100

    commit

---
+++ random generates.py
@@ -0,0 +1,8 @@
+import random
+your_number = eval(input("Enter the number you guess fro 10 - 99: "))
+
+random_number = random.randint(10, 99)
+print(random_number, "is the original number")
+
+
+
--- random_generate.py
+++ random_generate.py
@@ -1,5 +1,5 @@
 import random
-your_number = eval(input("Enter your guess between 111 and 999: "))
+your_number = eval(input("Enter the sum of the number from 1 - 27: "))
 
 random_number = random.randint(100, 999)
 print(random_number,"is the original number")
@@ -12,15 +12,7 @@ random_number_dig3 = random_number % 10
 random_sum = random_number_dig1 + random_number_dig2 + random_number_dig3
 print(random_sum,"\n")
 
-your_number_dig1 = your_number // 100
-your_number_dig2 = (your_number // 10) % 10
-your_number_dig3 = your_number  % 10
+print(your_number,"is the sum of the original number you guessed")
 
-print(your_number,"is the number you guessed")
-print("The sum of the number you guessed is", end = " ")
-
-your_number_sum = your_number_dig1 + your_number_dig2 + your_number_dig3
-print(your_number_sum,"\n")
-
-print("The sum of the number you guessed with the original number  is correct >>" if your_number_sum == random_sum else
+print("The sum of the number you guessed with the original number  is correct >>" if your_number == random_sum else
       "The sum of the number you guessed with the original is incorrect")
