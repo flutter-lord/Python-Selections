d53b7c9d5d40794eacd4658fe7a7dea741bb13fd
flutter-lord <richardolojede2@gmail.com>
2025-10-31 20:51:45 +0100

    commit

--- palindrome.py
+++ palindrome.py
@@ -5,5 +5,5 @@ num2 = (number // 10) % 10
 num3 = number % 10
 
 is_palindrome = num1 == num3
-print(is_palindrome)
+
 print(number,"is palindrome" if int(is_palindrome) > 0  else "is not palindrome")
