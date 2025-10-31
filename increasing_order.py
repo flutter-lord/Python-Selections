b0bba5274af6368fad36efe994e2ee28c07cf225
flutter-lord <richardolojede2@gmail.com>
2025-10-31 22:27:02 +0100

    commit

--- increasing_order.py
+++ increasing_order.py
@@ -2,11 +2,15 @@ num1, num2, num3 = eval(input("Enter three integers, and i'll help you arrange i
 
 max_num = max(num1, num2, num3)
 min_num = min(num1, num2, num3)
+middle_num = 0
 
+if (num1 > min_num) and (num1 < max_num):
+    middle_num = num1
 
+elif (num2 > min_num) and (num2 < max_num):
+    middle_num = num2
 
-if (num1 or num2 or num3 > min_num) and (num1 or num2 or num3 < max_num):
-    middle_num = num1 or num2 or num3
+else:
+    middle_num = num3
 
-
-print(num1,num2,num3, "in ascending order is", min_num, middle_num, max_num)
+print(num1,num2,num3, "in ascending order is",min_num,",",middle_num,",",max_num)
