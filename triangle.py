983bd3e78c565b89a0b571b057679cc7d795e76e
flutter-lord <richardolojede2@gmail.com>
2025-10-31 19:03:44 +0100

    commit

--- triangle.py
+++ triangle.py
@@ -1,8 +1,8 @@
 x1, x2, x3 = eval(input("Enter the lengths of the three sides of the triangle: "))
 
 if (x1 + x2 > x3) and (x1 + x3 > x2) and (x2 + x3 > x1):
-    print("The triangle is equilateral triangle")
+    print("The triangle is has valid lengths")
     print(x1 + x2 + x3,"is the perimeter of the triangle")
 
 else:
-    print("The input is invalid for an equilateral triangle")
+    print("The input is invalid")
