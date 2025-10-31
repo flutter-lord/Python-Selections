76a97bbb4456cac4ea6d52841d41e4ab70674748
flutter-lord <richardolojede2@gmail.com>
2025-10-31 18:22:00 +0100

    commit

---
+++ dollar_yuan_converter.py
@@ -0,0 +1,24 @@
+decision = eval(input("Enter 0 to convert dollars to CHY and 1 vice versa: "))
+
+if decision > 1:
+    print("input 0 and 1 only")
+    decision = eval(input("Enter 0 to convert dollars to CHY and 1 vice versa: "))
+
+if decision == 0:
+    print("You are converting USD(US dollar) to CHY(Chinese yuan)\n")
+
+    dollar = eval(input("Enter the dollar: "))
+    yuan = 6.81 * dollar
+
+    print(dollar,"dollar(USD) is equals to", yuan,"yuan(CHY)")
+
+elif decision == 1:
+    print("You are converting CHY(Chinese yuan) to USD(US dollar)\n")
+    yuan = eval(input("Enter the  yuan: "))
+    dollar = yuan / 6.81
+
+    print(yuan,"yuan(CHY) is equals to", dollar,"dollar(USD)")
+
+else:
+    print("input 0 and 1 only")
+    decision = eval(input("Enter 0 to convert dollars to CHY and 1 vice versa: "))
