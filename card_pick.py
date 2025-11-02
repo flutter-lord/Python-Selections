760f2055cfcebc028809bdfbd35585698df233d6
flutter-lord <richardolojede2@gmail.com>
2025-11-02 15:38:51 +0100

    commit

---
+++ card_pick.py
@@ -0,0 +1,176 @@
+import random
+
+rand1 = random.randint(1, 13)
+rand2 = random.randint(1, 4)
+
+if rand1 == 1:
+    if rand2 == 1:
+        print("The card you picked is Ace of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is Ace of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is Ace of Hearts")
+
+    else:
+        print("The card you picked is Ace of Spades")
+
+if rand1 == 2:
+    if rand2 == 1:
+        print("The card you picked is 2 of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is 2 of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is 2 of Hearts")
+
+    else:
+        print("The card you picked is 2 of Spades")
+
+if rand1 == 3:
+    if rand2 == 1:
+        print("The card you picked is 3 of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is 3 of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is 3 of Hearts")
+
+    else:
+        print("The card you picked is 3 of Spades")
+
+if rand1 == 4:
+    if rand2 == 1:
+        print("The card you picked is 4 of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is 4 of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is 4 of Hearts")
+
+    else:
+        print("The card you picked is 4 of Spades")
+
+if rand1 == 1:
+    if rand2 == 5:
+        print("The card you picked is 5 of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is 5 of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is 5 of Hearts")
+
+    else:
+        print("The card you picked is 5 of Spades")
+
+
+if rand1 == 6:
+    if rand2 == 1:
+        print("The card you picked is 6 of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is 6 of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is 6 of Hearts")
+
+    else:
+        print("The card you picked is 6 of Spades")
+
+if rand1 == 7:
+    if rand2 == 1:
+        print("The card you picked is 7 of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is 7 of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is 7 of Hearts")
+
+    else:
+        print("The card you picked is 7 of Spades")
+
+if rand1 == 8:
+    if rand2 == 1:
+        print("The card you picked is 8 of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is 8 of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is 8 of Hearts")
+
+    else:
+        print("The card you picked is 8 of Spades")
+
+if rand1 == 9:
+    if rand2 == 1:
+        print("The card you picked is 9 of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is 9 of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is 9 of Hearts")
+
+    else:
+        print("The card you picked is 9 of Spades")
+
+if rand1 == 10:
+    if rand2 == 1:
+        print("The card you picked is 10 of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is 10 of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is 10 of Hearts")
+
+    else:
+        print("The card you picked is 10 of Spades")
+
+
+if rand1 == 11:
+    if rand2 == 1:
+        print("The card you picked is Jack of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is Jack of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is Jack of Hearts")
+
+    else:
+        print("The card you picked is Jack of Spades")
+
+
+if rand1 == 12:
+    if rand2 == 1:
+        print("The card you picked is Queen of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is Queen of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is Queen of Hearts")
+
+    else:
+        print("The card you picked is Queen of Spades")
+
+if rand1 == 13:
+    if rand2 == 1:
+        print("The card you picked is King of Club")
+
+    elif rand2 == 2:
+        print("The card you picked is King of Diamonds")
+
+    elif rand2 == 3:
+        print("The card you picked is King of Hearts")
+
+    else:
+        print("The card you picked is King of Spades")
